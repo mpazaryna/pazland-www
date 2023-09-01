d0d03cfa521d34a52404f2d8d3f7f68395f4ff21
mpazaryna <mpazbot@gmail.com>
2023-09-01 17:11:08 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        This is the text.
+        Matthew Pazaryna email: matthew@pazland
       </div>
     </main>
   )
