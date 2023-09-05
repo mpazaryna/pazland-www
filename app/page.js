d61901cf27561fa6bb47c78217c6c4d31d8bacac
mpazaryna <mpazbot@gmail.com>
2023-09-05 13:21:15 -0400

    feat: adding jen art

--- app/page.js
+++ app/page.js
@@ -15,10 +15,10 @@ export default function Home() {
        
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/image-2.jpg"
+          alt="pazland"
+          width={580}
+          height={237}
           priority
         />
       </div>
