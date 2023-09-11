b461cf3a49b70ebcea75e87e9e09cd34b652ab5e
mpazaryna <mpazbot@gmail.com>
2023-09-11 15:47:49 -0400

    chore: adding chatbot

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d, t) {
+                var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+                v.onload = function() {
+                  window.voiceflow.chat.load({
+                    verify: { projectID: '64ff552876fbde0007df1be2' },
+                    url: 'https://general-runtime.voiceflow.com',
+                    versionID: 'production'
+                  });
+                }
+                v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+              })(document, 'script');
+            `
+          }}
+        />
+    </body>
     </html>
   )
 }
